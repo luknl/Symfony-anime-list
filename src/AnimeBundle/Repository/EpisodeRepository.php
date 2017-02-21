808a84dabe374a3aea2b04af4a86e4d3976ef167
Aude <aude.ficheux@hetic.net>
2017-02-21 19:13:47 +0100

    Front episodes in anime detail

--- src/AnimeBundle/Repository/EpisodeRepository.php
+++ src/AnimeBundle/Repository/EpisodeRepository.php
@@ -12,7 +12,7 @@ class EpisodeRepository extends \Doctrine\ORM\EntityRepository
 {
     public function findAllById($id)  {
         return $this->getEntityManager()
-            ->createQuery('SELECT p FROM AnimeBundle:Episode p WHERE p.anime = :id ORDER BY p.name ASC'  )
+            ->createQuery('SELECT p FROM AnimeBundle:Episode p WHERE p.anime = :id ORDER BY p.aired ASC'  )
             ->setParameter('id', $id)
             ->getResult();
     }
